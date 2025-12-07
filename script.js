52a988fffc85b48cfccac6fdd4754b730d24668c
chiragp234901 <chiragp2303@gmail.com>
2025-12-07 11:53:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
 
   var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState === 4 && this.status === 200) {
         // Typical action to be performed when the document is ready:
         //document.getElementById("demo").innerHTML = xhttp.responseText;
-        var response = JSON.parse(xhttp.responseText);
+        var response;
+        try {
+            response = JSON.parse(xhttp.responseText); // Attempt to parse JSON safely
+        } catch (error) {
+            console.error('Failed to parse JSON:', error);
+            response = null; // Fallback value or handle error accordingly
+        }
         //console.log(response);
         
-        var output = '';
-        var averageTotal = 0;
-        for (var i = 0; i < response.length; i++){
-            output += '<li class="scoreBox">';
-            output += '<ul id = "scoreBox' + i + '">'; 
-            output += '<li id="icon'+ i +'">'+'<img src ='+response[i].icon+' alt="iconImage">'+'</li>';
-            output += '<li id="category'+ i +'">'+ response[i].category + '</li>';
-            output += '<li id="score'+ i +'">'+ response[i].score + '<span class="outOf100"> / 100 </span>' + '</li>';
-            output += '</ul>';
-            output += '</li>';
-            averageTotal += response[i].score;
+        let output = '';
+        let averageTotal = 0;
+        if (Array.isArray(response)) { // Check if response is an array
+            for (let i = 0; i < response.length; i++){
+                output += '<li class="scoreBox">';
+                output += '<ul id = "scoreBox' + i + '">'; 
+                output += '<li id="icon'+ i +'">'+'<img src ='+response[i].icon+' alt="iconImage">'+'</li>';
+                output += '<li id="category'+ i +'">'+ response[i].category + '</li>';
+                output += '<li id="score'+ i +'">'+ response[i].score + '<span class="outOf100"> / 100 </span>' + '</li>';
+                output += '</ul>';
+                output += '</li>';
+                averageTotal += response[i].score;
+                }
+            } else {
+                console.error('Expected response to be an array but got:', response);
+                // Optionally, show a user-friendly message or fallback UI here
             }
             document.getElementById('score').innerHTML = output;
 
-            var displayAverage = Math.trunc(averageTotal / response.length);
+            let displayAverage = Math.trunc(averageTotal / response.length);
             document.getElementById('average').innerHTML = displayAverage;
 
-            display65Message = '';
-            display50Message = '';
-            displayGreatMessage = '';
-            displayWellDoneMessage = '';
+            let display65Message = '';
+            let display50Message = '';
+            let displayGreatMessage = '';
+            let displayWellDoneMessage = '';
 
             displayGreatMessage += '<p class ="greatMessage"> Great </p>'
             display65Message += '<p class ="65Message"> You scored higher than 65% of the people who have taken these tests.</p>'
